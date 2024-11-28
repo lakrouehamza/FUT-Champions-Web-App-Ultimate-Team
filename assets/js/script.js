2dee39fca07888f4d025bde3fac980e81bda6bdf
lakrouehamza <lakrounehamza10@gmail.com>
2024-11-28 21:57:10 +0100

    Gérer les validations des champs du formulaire.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,18 +16,18 @@
 // }
 // }
 
-var equepe =[];
+var equipe =[];
 
 
  
 let position = { position:[], RW : ['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'],LW: ['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'],ST :['PAC' , 'SHO' , 'PAS' , 'DRI' , 'DEF' , 'PHY'],
-                CDM :['PAC', 'PAS', 'DEF', 'PHY'],CM :['PAC', 'PAS', 'DEF', 'PHY'],LM :['PAC', 'PAS', 'DEF', 'PHY', 'DRI'],RM :['PAC', 'PAS', 'DEF', 'PHY', 'DRI'],
-                CB :['PAC', 'DEF', 'PHY'], LB :['PAC', 'DEF', 'PHY', 'PAS'], RB : ['PAC', 'DEF', 'PHY', 'PAS'],GK : ['DIV', 'HAN', 'KIC', 'REF', 'SPD', 'POS']};
+                CDM :['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'],CM :['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'],LM :['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'],RM :['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'],
+                CB :['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'], LB :['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'], RB : ['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'],GK : ['DIV', 'HAN', 'KIC', 'REF', 'SPD', 'POS']};
                 
 
 
  document.getElementById('containerCompetence').classList.add('hidden');
-document.getElementById('position').onclick =function(){ 
+ document.getElementById('position').addEventListener('change', function(){ 
     // console.log(position[document.getElementById('position').value]);
     if(document.getElementById('position').value=="position"){
         document.getElementById('containerCompetence').classList.add('hidden');
@@ -36,8 +36,8 @@ document.getElementById('position').onclick =function(){
     }
     let tr =document.createElement('tr'); 
     let trInput =document.createElement('tr'); 
-       tr.classList.add="dark:text-white";
-       document.querySelector('#competence ').innerHTML='';
+       tr.classList.add("dark:text-white");
+       document.querySelector('#competence').innerHTML = '';
        document.getElementById('competence').appendChild(tr);
        document.getElementById('competence').appendChild(trInput);
        let i=0;
@@ -60,56 +60,62 @@ document.getElementById('position').onclick =function(){
     }
 
        );
-};
+});
 
 function afichage(){
-    let cards = document.querySelectorAll('.carte');
 
-    // equepe.forEach(element =>{
-        cards.forEach(cardElement =>{
-            console.log(cardElement);
-            // if(cardElement.hasChildNodes()){
-            //     let div1 = document.createElement('div');
-            //     let div2 = document.createElement('div');
-            //     let div3 = document.createElement('div');
-            //     let spanPosition = document.createElement('span');
-            //     let span1 = document.createElement('span');
-            //     let span2 = document.createElement('span');
-            //     let table = document.createElement('table');
-            //     let trTitle = document.createElement('tr');
-            //     let trValue= document.createElement('tr');
-            //     let imgclup = document.createElement('img');
-            //     let imgJoure = document.createElement('img');
-            //     div2.classList.add('col-start-1','col-end-3','row-start-1','row-end-6','flex','flex-col','justify-between');
-            //     span1.classList.add('absolute','mt-2');
-            //     span2.classList.add('absolute','mt-7','text-[10px]');
-            //     imgJoure.classList.add('col-start-3','col-end-13','row-start-2','row-end-13');
-            //     div3.classList.add('rounded-xl','col-start-1','col-end-10','row-start-6','row-end-9','text-[6px]','flex','justify-center','items-center','bg-white/30');
-            //     spanPosition.classList.add('col-start-5','col-end-5','row-start-9','row-end-9','text-[9px]','mt-[-5px]');
-            //     imgJoure.classList.add('col-start-3','col-end-13','row-start-2','row-end-13');
-            //     cardElement.appendChild(div2);
-            //     cardElement.appendChild(imgJoure);
-            //     cardElement.appendChild(div3);
-            //     cardElement.appendChild(spanPosition);
-            //     div2.appendChild(span1);
-            //     div2.appendChild(span2);
-            //     div3.appendChild(table);
-            //     table.appendChild(trTitle);
-            //     table.appendChild(trValue);
-            //     span1.innerHTML=' ff';
-            //     span2.innerHTML= 'ff ';
-            //     span3.innerHTML='ggg';
-            //     for(let i = 0 ;i<6;i++){
-            //         let thTitre = document.createElement('th');
-            //         let thValue = document.createElement('th');
-            //         trTitle.appendChild(thTitre);
-            //         trValue.appendChild(thValue);
-            //         trTitle.innerText=' fr ';
-            //         trTitle.innerText= 'fr ';
-            //     }
-            // }
-        });
-    // });
+    const lesCards= document.querySelectorAll('.carte');
+
+    lesCards.forEach(element =>{
+        equipe.forEach(jouer =>{
+    if(element.hasChildNodes()){
+    console.log("ajoute a changement");
+    }
+    else
+    if(element.id == jouer.position){
+
+        console.log(jouer);
+        console.log("ajoute a tirane ");
+        let div1 = document.createElement('div');
+        let div2 = document.createElement('div');
+        let div3 = document.createElement('div');
+        let spanPosition = document.createElement('span');
+        let span1 = document.createElement('span');
+        let span2 = document.createElement('span');
+        let table = document.createElement('table');
+        let trTitle = document.createElement('tr');
+        let trValue= document.createElement('tr');
+        // let imgclup = document.createElement('img');
+        let imgJoure = document.createElement('img');
+        div2.classList.add('col-start-1','col-end-3','row-start-1','row-end-6','flex','flex-col','justify-between');
+        span1.classList.add('absolute','mt-2');
+        span2.classList.add('absolute','mt-7','text-[10px]');
+        // imgJoure.classList.add('col-start-3','col-end-13','row-start-2','row-end-13');
+        div3.classList.add('rounded-xl','col-start-1','col-end-10','row-start-6','row-end-9','text-[6px]','flex','justify-center','items-center','bg-white/30');
+        spanPosition.classList.add('col-start-5','col-end-5','row-start-9','row-end-9','text-[9px]','mt-[-5px]');
+        imgJoure.classList.add('col-start-3','col-end-13','row-start-2','row-end-13');
+        element.appendChild(div2);
+        element.appendChild(imgJoure);
+        element.appendChild(div3);
+        element.appendChild(spanPosition);
+        div2.appendChild(span1);
+        div2.appendChild(span2);
+        div3.appendChild(table);
+        table.appendChild(trTitle);
+        table.appendChild(trValue);
+        for(let i = 0 ;i<6;i++){
+            let thTitre = document.createElement('th');
+            let thValue = document.createElement('th');
+            trTitle.appendChild(thTitre);
+            trValue.appendChild(thValue);
+            trTitle.innerText=' fr ';
+            trTitle.innerText= 'fr ';
+        }
+
+        
+    }
+    });
+    });
 }
 afichage();
 
@@ -142,10 +148,8 @@ document.querySelectorAll("form input ,form select").forEach(element=>{
     let attribut =element.id;
     objetJoueur[attribut] = element.value;
 });
-equepe.push(objetJoueur);
+equipe.push(objetJoueur);
 // console.log(objetJoueur);
-// afichage();
+afichage();
 }
-
-
 });
