8582052429fd34467de0f5c69a8bad9990eed112
lakrouehamza <lakrounehamza10@gmail.com>
2024-11-29 04:15:20 +0100

    Écrire le code pour adapter automatiquement les postes des joueurs selon la formation tactique.

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,7 @@
 //     // liste[2].querySelector('img').remove();
     // console.log(liste);
 // }
-// }
+// }//Club , DEF , DRI, PAC, PAS, PHY,SHO, name,nationality,photo,position 
 
 var equipe =[];
 
@@ -65,54 +65,70 @@ let position = { position:[], RW : ['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'],LW
 function afichage(){
 
     const lesCards= document.querySelectorAll('.carte');
+    const changement =document.getElementById('changement');
 
     lesCards.forEach(element =>{
         equipe.forEach(jouer =>{
+            
+            let div1 = document.createElement('div');
+            let div2 = document.createElement('div');
+            let div3 = document.createElement('div');
+            let spanPosition = document.createElement('span');
+            let span1 = document.createElement('span');
+            let span2 = document.createElement('span');
+            let table = document.createElement('table');
+            let trTitle = document.createElement('tr');
+            let trValue= document.createElement('tr');
+            // let imgclup = document.createElement('img');
+            let imgJoure = document.createElement('img');
+            div1.classList.add('carte','grid','grid-cols-9','grid-rows-9','text-white');
+            div2.classList.add('col-start-1','col-end-3','row-start-1','row-end-6','flex','flex-col','justify-between');
+            span1.classList.add('absolute','mt-2');
+            span2.classList.add('absolute','mt-7','text-[10px]'); 
+            div3.classList.add('rounded-xl','col-start-1','col-end-10','row-start-6','row-end-9','text-[6px]','flex','justify-center','items-center','bg-white/30');
+            spanPosition.classList.add('col-start-5','col-end-5','row-start-9','row-end-9','text-[9px]','mt-[-5px]');
+            imgJoure.classList.add('col-start-3','col-end-13','row-start-2','row-end-13');
+            
+            div2.appendChild(span1);
+            div2.appendChild(span2);
+            div3.appendChild(table);
+            table.appendChild(trTitle);
+            table.appendChild(trValue);
+            // span1.textContent=jouer.
+            // span2.textContent=jouer.
+            spanPosition.textContent=jouer.position; 
+            imgJoure.src=jouer.photo;
+            for(let i = 0 ;i<6;i++){
+                let thTitre = document.createElement('th');
+                let thValue = document.createElement('th');
+                trTitle.appendChild(thTitre);
+                trValue.appendChild(thValue);
+                const key = Object.keys(jouer)[i+3];
+                thTitre.innerText=key;
+                thValue.innerText= jouer[key];
+            }        
+
     if(element.hasChildNodes()){
     console.log("ajoute a changement");
+    changement.appendChild(div1);
+    
+    div1.appendChild(div2);
+    div1.appendChild(imgJoure);
+    div1.appendChild(div3);
+    div1.appendChild(spanPosition);
+   
     }
     else
     if(element.id == jouer.position){
 
         console.log(jouer);
         console.log("ajoute a tirane ");
-        let div1 = document.createElement('div');
-        let div2 = document.createElement('div');
-        let div3 = document.createElement('div');
-        let spanPosition = document.createElement('span');
-        let span1 = document.createElement('span');
-        let span2 = document.createElement('span');
-        let table = document.createElement('table');
-        let trTitle = document.createElement('tr');
-        let trValue= document.createElement('tr');
-        // let imgclup = document.createElement('img');
-        let imgJoure = document.createElement('img');
-        div2.classList.add('col-start-1','col-end-3','row-start-1','row-end-6','flex','flex-col','justify-between');
-        span1.classList.add('absolute','mt-2');
-        span2.classList.add('absolute','mt-7','text-[10px]');
-        // imgJoure.classList.add('col-start-3','col-end-13','row-start-2','row-end-13');
-        div3.classList.add('rounded-xl','col-start-1','col-end-10','row-start-6','row-end-9','text-[6px]','flex','justify-center','items-center','bg-white/30');
-        spanPosition.classList.add('col-start-5','col-end-5','row-start-9','row-end-9','text-[9px]','mt-[-5px]');
-        imgJoure.classList.add('col-start-3','col-end-13','row-start-2','row-end-13');
         element.appendChild(div2);
         element.appendChild(imgJoure);
         element.appendChild(div3);
         element.appendChild(spanPosition);
-        div2.appendChild(span1);
-        div2.appendChild(span2);
-        div3.appendChild(table);
-        table.appendChild(trTitle);
-        table.appendChild(trValue);
-        for(let i = 0 ;i<6;i++){
-            let thTitre = document.createElement('th');
-            let thValue = document.createElement('th');
-            trTitle.appendChild(thTitre);
-            trValue.appendChild(thValue);
-            trTitle.innerText=' fr ';
-            trTitle.innerText= 'fr ';
-        }
-
         
+      
     }
     });
     });
@@ -149,7 +165,7 @@ document.querySelectorAll("form input ,form select").forEach(element=>{
     objetJoueur[attribut] = element.value;
 });
 equipe.push(objetJoueur);
-// console.log(objetJoueur);
+console.log(objetJoueur);
 afichage();
 }
 });
