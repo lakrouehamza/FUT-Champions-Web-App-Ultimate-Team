b60f2c8b205e753e544d3ad379885da509ff2814
lakrouehamza <lakrounehamza10@gmail.com>
2024-12-03 09:11:08 +0100

    Ajouter une fonction pour afficher et fermer le modal

--- script.js
+++ script.js
@@ -2,15 +2,31 @@
 let lesCards =document.getElementById('tirane').querySelectorAll('.cardAjout');
 lesCards.forEach(card => {
 card.addEventListener('click',function(){
-if(card.hasChildNodes()){
-    console.log(card.parentNode.querySelector('#modifyCard'));    
+if(card.hasChildNodes()){   
     if(card.parentNode.querySelector('#modifyCard').classList.contains('hidden'))
         card.parentNode.querySelector('#modifyCard').classList.remove('hidden');
     else 
         card.parentNode.querySelector('#modifyCard').classList.add('hidden');
 }
+});
+card.parentNode.querySelector('#modifyCard').querySelector('img[src="./assets/img/edit.png"]').addEventListener('click', function(){
+    console.log('edit')
+});
 
+card.parentNode.querySelector('#modifyCard').querySelector('img[src="./assets/img/change.png"]').addEventListener('click', function(){
+    console.log('change')
+});
+
+card.parentNode.querySelector('#modifyCard').querySelector('img[src="./assets/img/de.webp"]').addEventListener('click', function(){
+    console.log('delete')
 });
 });
 
+function fermeModal(){
+    if(document.getElementById('modal').classList.contains('hidden')){
+        document.getElementById('modal').classList.remove('hidden');
+    }
+    else 
+        document.getElementById('modal').classList.add('hidden');
+}
 
